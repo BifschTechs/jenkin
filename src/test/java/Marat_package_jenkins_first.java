811be9d59fb2137ba918d77fc2ed507f79feab71
Mara Plus <maraplus@Maras-MacBook-Pro.local>
2019-07-15 20:36:03 -0500

    MyFirstMaraplus

---
+++ src/test/java/Marat_package_jenkins_first.java
@@ -0,0 +1,10 @@
+public class Marat_package_jenkins_first {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I am expert in Jenkins");
+        }
+    }
+}
